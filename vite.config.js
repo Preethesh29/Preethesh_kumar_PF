70ea119045f19d7d30d7c0d6add332fa02eca737
Preethesh29 <preethesh.kumar29@gmail.com>
2026-03-16 19:03:20 +0530

    Configure base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Preethesh_kumar_PF/',
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg'],
   server: {
